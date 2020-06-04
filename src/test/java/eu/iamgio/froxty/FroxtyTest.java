23e23d831042ff0cf6a1fb20db298209dc3e7665
Giorgio Garofalo <giogar2705@gmail.com>
2020-06-04 15:50:39 +0100

    Update tests: add opacity slider

--- src/test/java/eu/iamgio/froxty/FroxtyTest.java
+++ src/test/java/eu/iamgio/froxty/FroxtyTest.java
@@ -1,9 +1,15 @@
 package eu.iamgio.froxty;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 /**
@@ -15,34 +21,51 @@ public class FroxtyTest extends Application {
         AnchorPane root = new AnchorPane();
         Scene scene = new Scene(root, 600, 600);
         scene.getStylesheets().add("/teststyle.css");
+        Font.loadFont(getClass().getResourceAsStream("/font/Karla-Regular.ttf"), 16);
 
         root.getStyleClass().add("root");
         root.prefWidthProperty().bind(scene.widthProperty());
         root.prefHeightProperty().bind(scene.heightProperty());
 
-        setupEffect(root);
+        FrostyEffect effect = setupEffect(root);
+        setupSliders(root, effect);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("FroXty test");
         primaryStage.show();
     }
 
-    private void setupEffect(Pane root) {
-        Pane pane = new Pane();
+    private FrostyEffect setupEffect(Pane root) {
+        Label label = new Label("Welcome to\nFroXty");
+        label.setTextAlignment(TextAlignment.CENTER);
+        label.setAlignment(Pos.CENTER);
+
+        Pane target = new Pane(label);
 
         FrostyEffect effect = new FrostyEffect();
 
-        pane.prefWidthProperty().bind(root.prefWidthProperty().divide(2));
-        pane.prefHeightProperty().bind(root.prefHeightProperty().divide(4));
+        target.prefWidthProperty().bind(root.prefWidthProperty().divide(2));
+        target.prefHeightProperty().bind(root.prefHeightProperty().divide(4));
 
-        effect.apply(pane);
+        label.prefWidthProperty().bind(target.prefWidthProperty());
+        label.prefHeightProperty().bind(target.prefHeightProperty());
+
+        effect.apply(target);
         FrostyBox box = effect.getBox();
 
         box.translateXProperty()
-                .bind(root.prefWidthProperty().divide(2).subtract(pane.prefWidthProperty().divide(2)));
+                .bind(root.prefWidthProperty().divide(2).subtract(target.prefWidthProperty().divide(2)));
         box.translateYProperty()
-                .bind(root.prefHeightProperty().divide(2).subtract(pane.prefHeightProperty().divide(2)));
+                .bind(root.prefHeightProperty().divide(2).subtract(target.prefHeightProperty().divide(2)));
 
         root.getChildren().add(box);
+        return effect;
+    }
+
+    private void setupSliders(Pane root, FrostyEffect effect) {
+        Slider opacitySlider = new Slider(0, 1, .5);
+        effect.opacityProperty().bind(opacitySlider.valueProperty());
+
+        root.getChildren().add(new VBox(opacitySlider));
     }
 }
