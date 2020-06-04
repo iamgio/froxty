696b06514914a508ca14a4decff45193d4fbebc3
Giorgio Garofalo <giogar2705@gmail.com>
2020-06-04 12:48:19 +0100

    Add tests

--- src/test/java/eu/iamgio/froxty/FroxtyTest.java
+++ src/test/java/eu/iamgio/froxty/FroxtyTest.java
@@ -3,6 +3,7 @@ package eu.iamgio.froxty;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -10,11 +11,38 @@ import javafx.stage.Stage;
  */
 public class FroxtyTest extends Application {
 
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         AnchorPane root = new AnchorPane();
         Scene scene = new Scene(root, 600, 600);
+        scene.getStylesheets().add("/teststyle.css");
+
+        root.getStyleClass().add("root");
+        root.prefWidthProperty().bind(scene.widthProperty());
+        root.prefHeightProperty().bind(scene.heightProperty());
+
+        setupEffect(root);
+
         primaryStage.setScene(scene);
         primaryStage.setTitle("FroXty test");
         primaryStage.show();
+    }
+
+    private void setupEffect(Pane root) {
+        Pane pane = new Pane();
+
+        FrostyEffect effect = new FrostyEffect();
+
+        pane.prefWidthProperty().bind(root.prefWidthProperty().divide(2));
+        pane.prefHeightProperty().bind(root.prefHeightProperty().divide(4));
+
+        effect.apply(pane);
+        FrostyBox box = effect.getBox();
+
+        box.translateXProperty()
+                .bind(root.prefWidthProperty().divide(2).subtract(pane.prefWidthProperty().divide(2)));
+        box.translateYProperty()
+                .bind(root.prefHeightProperty().divide(2).subtract(pane.prefHeightProperty().divide(2)));
+
+        root.getChildren().add(box);
     }
 }
